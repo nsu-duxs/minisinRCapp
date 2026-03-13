d0f20a9a026a6b050a5243cd63698169244b96ac
Arthur Soares <artssoares26@ufrj.br>
2026-03-13 14:43:07 -0300

    ADD: adicionei todos os parâmetros restantes

--- lib/main.dart
+++ lib/main.dart
@@ -32,48 +32,51 @@ class _BLEWriteAppState extends State<BLEWriteApp> {
   
   // 2. CADASTRE SEUS UUIDS AQUI
   final List<BleCommand> myCommands = [
-    BleCommand(name: "servo posicão", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "aff04f40-41ab-493c-925d-37f4b2d92325"),
+    // --- BLOCO SERVO (ZORRO) ---
+    BleCommand(name: "Servo Posição", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "aff04f40-41ab-493c-925d-37f4b2d92325"),
+    BleCommand(name: "Servo Inicial", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "31296a63-d6ce-4daf-a4a8-0f4d59907071"),
+    BleCommand(name: "Servo Ativado", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "01f0d89c-ed13-46db-98b3-e93d485fdc74", tipo: "bool"),
 
-    BleCommand(name: "Servo inicial", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "31296a63-d6ce-4daf-a4a8-0f4d59907071"),
+    // --- BLOCO VELOCIDADE E MARCHAS ---
+    BleCommand(name: "Min PWM", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "b1a10001-ce95-4ada-b8f5-9c63ff4e61ad"),
+    BleCommand(name: "Velocidade Máxima (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "b1a10002-ce95-4ada-b8f5-9c63ff4e61ad"),
+    BleCommand(name: "Limite Troca Marcha", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "b1a10003-ce95-4ada-b8f5-9c63ff4e61ad"),
 
-    BleCommand(name: "Servo Ativado", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "01f0d89c-ed13-46db-98b3-e93d485fdc74",  tipo: "bool"),
+    // --- BLOCO GIROS (TEMPOS) ---
+    BleCommand(name: "Giro 45 Anti-Horário", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "aed55e60-5927-4d22-ad1c-c2135096df70"),
+    BleCommand(name: "Giro 45 Horário", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "17748c8a-2d2e-4a18-8054-6dd6db805d61"),
+    BleCommand(name: "Giro 90 Anti-Horário", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "c238b3c8-27eb-455a-9eb7-26162b538a45"),
+    BleCommand(name: "Giro 90 Horário", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "48e04e0d-f49c-4d1f-b3c1-51cdd10cbd65"),
 
-    BleCommand(name: "giro45 Anti Horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "aed55e60-5927-4d22-ad1c-c2135096df70"),
+    // --- BLOCO ARCOS (TEMPOS E VELOCIDADES) ---
+    BleCommand(name: "Arco Anti-Horário (Tempo)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "ebd8a9e4-085a-41ed-bd4f-0e46cbd1c4b3", tipo: "int16"),
+    BleCommand(name: "Velo. Esq. Arco Anti (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "v001a490-ce95-4ada-b8f5-9c63ff4e61ad"),
+    BleCommand(name: "Arco Horário (Tempo)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "866001fe-5690-4e56-96fc-ce17687f0d5d", tipo: "int16"),
+    BleCommand(name: "Velo. Dir. Arco Hor (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "v002a490-ce95-4ada-b8f5-9c63ff4e61ad"),
 
-    BleCommand(name: "giro45 Horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "17748c8a-2d2e-4a18-8054-6dd6db805d61"),
+    BleCommand(name: "Arco Shikiri Anti (Tempo)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "7a2a2932-c806-4068-a3e3-e78e640a2a07"),
+    BleCommand(name: "Velo. Esq. Shikiri Anti (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "v003a490-ce95-4ada-b8f5-9c63ff4e61ad"),
+    BleCommand(name: "Arco Shikiri Hor (Tempo)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "3d0ac383-e16a-4e42-870d-f2a859efe8c0"),
+    BleCommand(name: "Velo. Dir. Shikiri Hor (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "v004a490-ce95-4ada-b8f5-9c63ff4e61ad"),
 
-    BleCommand(name: "giro 90 Anti Horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "c238b3c8-27eb-455a-9eb7-26162b538a45"),
+    BleCommand(name: "Arco Borda Anti (Tempo)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "7d049e22-20f6-4e01-b018-7e8fcf8ac91b"),
+    BleCommand(name: "Velo. Esq. Borda Anti (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "v005a490-ce95-4ada-b8f5-9c63ff4e61ad"),
+    BleCommand(name: "Arco Borda Hor (Tempo)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "7bd3ee6f-c6a8-4591-852a-93af450cb940"),
+    BleCommand(name: "Velo. Dir. Borda Hor (%)", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "v006a490-ce95-4ada-b8f5-9c63ff4e61ad"),
 
-    BleCommand(name: "giro 90 Horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "48e04e0d-f49c-4d1f-b3c1-51cdd10cbd65"),
+    // --- BLOCO ZIG-ZAG ---
+    BleCommand(name: "Zig-Zag Anti-Horário", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "65f2b4cc-cb29-4464-8695-f7d929fe3a79"),
+    BleCommand(name: "Zig-Zag Horário", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "22533c8f-130d-4775-8870-d4229cfd272d"),
+    BleCommand(name: "Zig-Zag Shikiri Anti", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "ab9a4df9-74a5-4f4c-bfd2-4e1c7d40ba74"),
+    BleCommand(name: "Zig-Zag Shikiri Hor", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "6e21271d-e8e9-4365-afad-66ebd5af0f28"),
+    BleCommand(name: "Zig-Zag Borda Anti", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "f7ad38dd-acb1-4a53-baec-6aa279f9f35b"),
+    BleCommand(name: "Zig-Zag Borda Hor", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "adbfa60f-3068-4cfb-b0c4-382977d1dc23"),
 
-    BleCommand(name: "arco anti-Horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "ebd8a9e4-085a-41ed-bd4f-0e46cbd1c4b3", tipo: "int16"),
-
-    BleCommand(name: "arco Horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "866001fe-5690-4e56-96fc-ce17687f0d5d", tipo:"int16"),
-    
-    BleCommand(name: "arco shikiri anti-horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "7a2a2932-c806-4068-a3e3-e78e640a2a07"),
-
-    BleCommand(name: "arco shikiri horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "3d0ac383-e16a-4e42-870d-f2a859efe8c0"),
-
-    BleCommand(name: "Arco borda anti-horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "7d049e22-20f6-4e01-b018-7e8fcf8ac91b"),
-
-    BleCommand(name: "Arco borda horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "7bd3ee6f-c6a8-4591-852a-93af450cb940"),
-
-    BleCommand(name: "zigzag anti-horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "65f2b4cc-cb29-4464-8695-f7d929fe3a79"),
-
-    BleCommand(name: "zigzag horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "22533c8f-130d-4775-8870-d4229cfd272d"),
-
-    BleCommand(name: "zigzag shikiri anti-horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "ab9a4df9-74a5-4f4c-bfd2-4e1c7d40ba74"),
-
-    BleCommand(name: "zigzag shikiri horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "6e21271d-e8e9-4365-afad-66ebd5af0f28"),
-
-    BleCommand(name: "zigzag borda horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "adbfa60f-3068-4cfb-b0c4-382977d1dc23"),
-
-    BleCommand(name: "zigzag borda anti-horario", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid:  "f7ad38dd-acb1-4a53-baec-6aa279f9f35b"),
-
-
-    // Adicione quantos quiser no mesmo modelo...
+    // --- BLOCO EXTRAS ---
+    BleCommand(name: "Controle Agressivo", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "b1a10004-ce95-4ada-b8f5-9c63ff4e61ad", tipo: "bool"),
+    BleCommand(name: "Tempo Stop Motors", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "b1a10005-ce95-4ada-b8f5-9c63ff4e61ad"),
+    BleCommand(name: "Inversão Motores", serviceUuid: "41a490f5-ce95-4ada-b8f5-9c63ff4e61ad", charUuid: "b1a10006-ce95-4ada-b8f5-9c63ff4e61ad", tipo: "bool"),
   ];
-
   // Comando selecionado no Dropdown
   late BleCommand selectedCommand;
   
